12b3c6a17ef45ed0d6acdf5f5e0b90877de7bafb
konurov <s_konurov@mail.ru>
2023-10-06 23:52:35 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+error_reporting(-1);
+/*$x = 55
+if($x < 10){
+    echo 'x больше нуля';
+}elseif($x == 10){
+echo "равно ";
+}
+if(NULL){
+    echo 'Условие выполнено';
+}else{
+    echo "Условие не выполнено";
+}*/
+/*
+$color = "yellow";
+if($color == "red"){
+    echo "We may stop";
+}elseif($color == "yellow"){
+    echo "We may ready";
+}elseif($color == "green"){
+    echo "We may go";
+}else{
+    echo "error";
+}*/
+/*$h = 3;
+
+switch($h){
+    case 1:
+        echo "Число равно 1";
+        break;
+    case 2:
+        echo "Число равно 2";
+        break;
+    default:
+        echo "Число не равно ни 1, ни 2";
+    
+}
+*/
+
+
+/*
+for($i=1, print 'Hi Loop is from Here'; $i<=10; print $i . ',', $i=$i+2);
+
+
+
+for($i = 0; $i <= 10; $i +=3) echo "Hello {$i}<br>";
+$summ = 1;
+$n = 10;
+for($i = 1; $i<=$n; $i++){
+  //  $summ += $i; //$summ + $i
+    echo "<br>";
+    echo $summ += $i;
+}
+//echo $summ;*/
+/*$i = 0;
+$i<=10;
+for(; ;){
+    if($i == 8) continue;
+    echo "$i <br>";
+    if($i == 7) break;
+    $i++;
+}*/
+for($i=1, print 'Hi Loop is from Here'; $i<=10; print $i . ',', $i=$i+5);
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
