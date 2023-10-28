e539176e259f6a3f2660adbdf1d7ccfcde2477bb
konurov <s_konurov@mail.ru>
2023-10-28 09:24:55 +0600

    my

---
+++ less03/index.php
@@ -0,0 +1,44 @@
+<?php
+error_reporting(-1);
+if(!empty($_FILES)){
+    echo "<pre>";
+        print_r($_FILES);
+    echo "</pre>";
+    move_uploaded_file($_FILES['photo']['tmp_name'],"uploads/".$_FILES['photo']['name']);
+
+$arr = array("Asan","Usyn","Sanjar","Mirbek");
+    echo "<pre>";
+        var_dump($arr);
+    echo "</pre>";
+    echo "<pre>";
+        print_r($arr);
+    echo "</pre>";
+   
+    echo $arr['2'];
+
+    $arr4 = ["Asan","Usyn","Sanjar",[200,201,203,204],"Mirbek"];
+
+    echo "<pre>";
+    print_r($arr4);
+echo "</pre>";
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form enctype="multipart/form-data" action="" method="post">
+        <input type="file" name="photo" id="">
+        <input type="submit" value="Ок">
+
+        
+    </form>
+
+    <h2><?php echo $arr4[2];  echo "----"; echo $arr4[3][2];?></h2>
+</body>
+</html>
