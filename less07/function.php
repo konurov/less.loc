73e763eb70dce621d646ced867f8c7323370225b
konurov <s_konurov@mail.ru>
2023-12-09 09:29:56 +0600

    my

--- less07/function.php
+++ less07/function.php
@@ -1,42 +1,39 @@
 <?php 
 function clear(){
-    global $conn;
+    global $db;
     foreach ($_POST as $key => $value) {
-		$_POST[$key] = mysqli_real_escape_string($conn, $value);
+		$_POST[$key] = mysqli_real_escape_string($db, $value);
 	}
 }
 function save_mess(){
-    global $conn;
+    global $db;
     clear();
     extract($_POST);
-    #$text = mysqli_real_escape_string($conn, $_POST['text']);
+    //$text = mysqli_real_escape_string($db, $_POST['text1']);
     $query = "INSERT INTO mess (text) VALUES('$text')";
-    mysqli_query($conn, $query);
+    mysqli_query($db, $query);
     
    
 }
 function get_mess(){
-    global $conn;
+    global $db;
     $query = "SELECT * FROM mess ORDER BY date DESC";
-    $res = mysqli_query($conn, $query);
+    $res = mysqli_query($db, $query);
     return mysqli_fetch_all($res, MYSQLI_ASSOC);
 
 }
-
 function tel_mess(){
     $text = $_POST['text'];
-    $apiToken = "6786168897:AAGkTJsi-de3zI7Ib1ndP-WRE2b3ksdHvXo";
+    $apiToken = "++";
     $data = [
-      'chat_id' => '790926957',
+      'chat_id' => '++',
       'text' => $text
     ];
   $response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?" .
                                  http_build_query($data) );
 }
-
 function show_arr($arr){
     echo '<pre>'.print_r($arr, true).'</pre>';
 }
-
 
 ?>
